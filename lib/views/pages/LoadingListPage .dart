fdd82a5e67d18047f2fbc4c09b583dc741eda872
MahirMk <110602304+MahirMk@users.noreply.github.com>
2022-11-19 17:57:59 +0530

    Add files via upload

---
+++ lib/views/pages/LoadingListPage .dart
@@ -0,0 +1,211 @@
+import 'package:apiinterview/views/pages/MyHomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class LoadingListPage  extends StatefulWidget {
+  @override
+  State<LoadingListPage > createState() => _LoadingListPageState();
+}
+
+class _LoadingListPageState extends State<LoadingListPage > {
+  bool _enabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Loading List'),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              child: Shimmer.fromColors(
+                baseColor: Colors.grey[300],
+                highlightColor: Colors.grey[100],
+                enabled: _enabled,
+                child: ListView.builder(
+                  itemBuilder: (_, __) => Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: 48.0,
+                          height: 48.0,
+                          color: Colors.white,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 2.0),
+                              ),
+                              Container(
+                                width: double.infinity,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 2.0),
+                              ),
+                              Container(
+                                width: 40.0,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  itemCount: 6,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _enabled = !_enabled;
+                    });
+                  },
+                  child: Text(
+                    _enabled ? 'Stop' : 'Play',
+                    style: Theme.of(context).textTheme.button.copyWith(
+                        fontSize: 18.0,
+                        color: _enabled ? Colors.redAccent : Colors.green),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SlideToUnlockPage extends StatelessWidget {
+  final List<String> days = <String>[
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  final List<String> months = <String>[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final DateTime time = DateTime.now();
+    final int hour = time.hour;
+    final int minute = time.minute;
+    final int day = time.weekday;
+    final int month = time.month;
+    final int dayInMonth = time.day;
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'img/pic3.jpg',
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 300.0,
+            right: 0.0,
+            left: 0.0,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '${hour < 10 ? '0$hour' : '$hour'}:${minute < 10 ? '0$minute' : '$minute'}',
+                    style: const TextStyle(
+                      fontSize: 60.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                   Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                  ),
+                  Text(
+                    '${days[day - 1]}, ${months[month - 1]} $dayInMonth',
+                    style: const TextStyle(fontSize: 24.0, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 24.0,
+              left: 0.0,
+              right: 0.0,
+              child: Center(
+                child: Opacity(
+                  opacity: 0.8,
+                  child: Shimmer.fromColors(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Image.asset(
+                          'img/pic.jpg',
+                          height: 20.0,
+                        ),
+                         Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                        ),
+                         GestureDetector(
+                           onTap: (){
+                             Navigator.of(context).push(
+                               MaterialPageRoute(builder: (context)=>MyHomePage())
+                             );
+                           },
+                           child: Text(
+                            'Slide to unlock',
+                            style: TextStyle(
+                              fontSize: 28.0,
+                              color: Colors.green,
+                            ),
+                        ),
+                         )
+                      ],
+                    ),
+                    baseColor: Colors.white,
+                    highlightColor: Colors.green,
+                    loop: 3,
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
